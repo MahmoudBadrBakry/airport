9b92991ed2a50b4f1c3267e7ced0869a0f58f3a8
Mahmoud Badr <mbadr@kn-it.com>
2022-08-30 23:40:52 +0200

    adding premiumFlight feature

--- src/main/java/com/pluralsight/tddjunit5/airport/PremuimFlight.java
+++ src/main/java/com/pluralsight/tddjunit5/airport/PremuimFlight.java
@@ -1,2 +1,20 @@
-package com.pluralsight.tddjunit5.airport;public class PremuimFlight {
+package com.pluralsight.tddjunit5.airport;
+
+public class PremuimFlight extends Flight {
+    public PremuimFlight(String id) {
+        super(id);
+    }
+
+    @Override
+    public boolean addPassenger(Passenger passenger) {
+        if (passenger.isVip()) {
+            return passengersList.add(passenger);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removePassenger(Passenger passenger) {
+        return passengersList.remove(passenger);
+    }
 }
--- src/test/java/com/pluralsight/tddjunit5/airport/AirportTest.java
+++ src/test/java/com/pluralsight/tddjunit5/airport/AirportTest.java
@@ -101,4 +101,53 @@ public class AirportTest {
         }
 
     }
+
+
+    @DisplayName("Given there is an premium flight")
+    @Nested
+    class PremiumClassFlightTest {
+        Flight premiumFlight;
+        Passenger vipPassenger;
+        Passenger passenger;
+
+        @BeforeEach
+        public void init() {
+            premiumFlight = new PremuimFlight("1");
+            vipPassenger = new Passenger("Mahmoud Badr", true);
+            passenger = new Passenger("ali", false);
+        }
+
+        @Nested
+        @DisplayName("when we have a usual passenger")
+        class UsualPassenger {
+            @Test
+            @DisplayName("try to add or remove the passenger")
+            public void premiumFlightTestUsualPassenger() {
+                assertAll("verify all conditions",
+                        () -> assertFalse(premiumFlight.addPassenger(passenger)),
+                        () -> assertFalse(premiumFlight.removePassenger(passenger)),
+                        () -> assertEquals(0, premiumFlight.getPassengersList().size()),
+                        () -> assertEquals("1", premiumFlight.getId())
+                );
+            }
+        }
+
+        @Nested
+        @DisplayName("when we have a vip passenger")
+        class VipPassenger {
+            @Test
+            @DisplayName("try to add or remove the passenger")
+            public void premiumFlightTestVipPassenger() {
+                assertAll("verify all conditions",
+                        () -> assertTrue(premiumFlight.addPassenger(vipPassenger), "vip passenger should be added"),
+                        () -> assertEquals("Mahmoud Badr", vipPassenger.getName()),
+                        () -> assertTrue(premiumFlight.removePassenger(vipPassenger)),
+                        () -> assertEquals(0, premiumFlight.getPassengersList().size()),
+                        () -> assertEquals("1", premiumFlight.getId())
+                );
+            }
+
+        }
+
+    }
 }
